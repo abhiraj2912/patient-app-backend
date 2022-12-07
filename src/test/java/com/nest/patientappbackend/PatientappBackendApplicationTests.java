399a547ef08c7dd6500e25d74bcd984069b23853
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-07 07:51:08 +0530

    first

---
+++ src/test/java/com/nest/patientappbackend/PatientappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.patientappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
